9d237c9637405c3b0d3f760851a8b9c1bd094120
Yasin Memon <memonyasin2007@gmail.com>
2024-06-16 22:13:13 +0530

    user authentication page comepeleted

--- AMMRUY-FRONTEND/src/App.jsx
+++ AMMRUY-FRONTEND/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Testimonials from './components/Testimonials' 
+import Authentication from './components/Authentication'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello World</h1>
+      {/* <Testimonials/> */}
+      <Authentication/>
     </>
   )
 }
