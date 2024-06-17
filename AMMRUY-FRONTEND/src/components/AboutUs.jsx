624f2aa323e575460e8ccfbf96bd4e2138e402bf
mayank4655 <mayankraj4655@gmail.com>
2024-06-18 00:49:41 +0530

    Updated AboutUs

--- AMMRUY-FRONTEND/src/components/AboutUs.jsx
+++ AMMRUY-FRONTEND/src/components/AboutUs.jsx
@@ -7,11 +7,11 @@ function AboutUs() {
     <div className="flex ">
       <div className="first w-full">
        <div className="relative flex flex-row m-10 my-24 ml-2 mt-[10rem]">
-            <img className="w-[600px] h-500px] m-10 ml-[2rem]" src={img2} alt="Electrical appliances" />
-            <img className="absolute w-[355px] h-[240px] mx-[35%] ml-[26rem] my-[23rem] mt-[12rem]" src={img1} alt="Washing maching repair" />
+            <img className="w-[650px] h-500px] m-10 ml-[2rem]" src={img2} alt="Electrical appliances" />
+            <img className="absolute w-[355px] h-[240px] mx-[35%] ml-[28.5rem] my-[23rem] mt-[14rem]" src={img1} alt="Washing maching repair" />
        </div>
       </div>
-      <div className="second w-full login-bg text-center w-[1200px]">
+      <div className="second login-bg text-center w-[1200px]">
         <div className="header mx-auto text-white  mt-[3rem]">
             <h1 className='font-bold text-5xl my-4 mb-[2rem]'>The Team</h1>
             <p className='mx-auto text-xl my-2 roboto-thin w-[80%] text-left'> We are dedicated to offering our clients fast, affordable and professional appliance repair service. Resolving the appliance repair issues of our customers in a timely and affordable price is our #1 goal.
