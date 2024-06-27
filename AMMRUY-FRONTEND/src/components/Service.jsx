9e6341bf54e2a1089c0844a44fb2efba5f8a7a98
Raj Pandey <rajpandey552002@gmail.com>
2024-06-27 20:14:09 +0530

    services responsive

--- AMMRUY-FRONTEND/src/components/Service.jsx
+++ AMMRUY-FRONTEND/src/components/Service.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Service() {
   return (
-    <div className='service flex justify-around min-h-screen m-auto'>
-      <div className="leftSide m-auto">
+    <div className='service flex justify-between items-center gap-4 min-h-screen mx-4 md: flex-col'>
+      <div className="leftSide h-[400px] w-[350px] md:w-[508px] m-auto">
         <img
-          className='h-[400px] w-[508px] rounded-xl'
+          className='h-full w-full rounded-xl'
           src="https://s3-alpha-sig.figma.com/img/748f/2a7f/bd1c1bc99e72dd08b1e3e47060b6502c?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PJBH9uj4fmz0Q5j9R2hju0PrLAabqlypq07IT4V-NSGD0Ax4JtG~2SDiZ1arKGCZEUoUeeXbnVWR81ceu3cHbFCmEDrhQ8jmcdrcjjHeMGmqRpqYqfYIt0w6iCJ4XmBsXWwr9hcGUKXJYb~eknTaBmePOpoGDOAfNopsv7jeTCSvgPZ5hbuYsqV3FgfehE2sK~JEJ5TURCwuoJEDW5iev2D4M9rW66ED34blDAkk~jE73WIhQe0NtoO-KrLDVUmHjWnDtMMGrYSG7h62Pto9d3Foayjd~5Ku32wT1MeUOhtrGoE3jLuIMugZ1F6Q2rPWCX2pvunqOXG9R4Hv7gohXA__" />
       </div>
       <div className="rightSide m-auto font-serif">
